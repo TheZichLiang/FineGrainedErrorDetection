2420087c97954528df1b3a9547e1fa0fa2027443
Spencer Robert Jenkins <c7230021@nexusclass01.umiacs.umd.edu>
2024-11-30 09:58:34 -0500

    Quick modification to script

--- script.sh
+++ script.sh
@@ -4,4 +4,4 @@ srun --qos=default --account=class --partition=class --gres=gpu:1 --mem=32g --ti
 Start jupyter notebook:
 srun --pty --qos=default --account=class --partition=class --gres=gpu:1 --mem=32g --time=01:00:00 bash
 jupyter notebook --no-browser --port=8889 --ip=0.0.0.0
-ssh -N -f -L localhost:8888:tron29.umiacs.umd.edu:8889 c7230021@nexusclass.umiacs.umd.edu
+ssh -N -f -L localhost:8888:$(hostname):8889 $USER@nexusclass.umiacs.umd.edu
